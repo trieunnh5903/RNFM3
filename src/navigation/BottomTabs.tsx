222610dd756f4c04ef84e56b32ff4c4dd4f9f01f
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2023-12-26 15:32:17 +0700

    .

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {BottomTabNavigatorParamList} from '../types/navigation.type';
 import {AccountScreen, DiscoveryScreen, PlayMusicScreen} from '../screen';
+import {Image, Text, View} from 'react-native';
+import {icons} from '../constant';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const Tab = createBottomTabNavigator<BottomTabNavigatorParamList>();
 
@@ -9,15 +12,43 @@ const BottomTabs = () => {
   return (
     <Tab.Navigator
       initialRouteName="PlayMusic"
-      screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Discovery" component={DiscoveryScreen} />
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: 'black',
+          borderColor: 'black',
+          height: 70,
+        },
+      }}>
       <Tab.Screen
-        name="PlayMusic"
-        component={PlayMusicScreen}
+        name="Discovery"
+        component={DiscoveryScreen}
         options={{
-          tabBarShowLabel: false,
+          tabBarIcon({focused}) {
+            return (
+              <View style={{alignItems: 'center'}}>
+                <Image
+                  style={{width: 32, height: 32}}
+                  source={
+                    focused
+                      ? icons.headphone_non_selected
+                      : icons.headphone_selected
+                  }
+                />
+                <Text
+                  style={{
+                    color: focused ? '#15CEFD' : '#A8A8A8',
+                    fontSize: 12,
+                  }}>
+                  Khám phá
+                </Text>
+              </View>
+            );
+          },
         }}
       />
+      <Tab.Screen name="PlayMusic" component={PlayMusicScreen} />
       <Tab.Screen name="Account" component={AccountScreen} />
     </Tab.Navigator>
   );
